dbf9a4b73890a7b3811e00b4b0e5029f973a50b8
nidalhashmi <148055110+nidalhashmi@users.noreply.github.com>
2023-12-07 19:01:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,63 @@
-resource "local_file" "pet" {
-  filename = "pets.txt"
-  content  = "we love pets!"
-}
-resource "random_pet" "mypet" {
-  prefix    = "MR"
-  separator = "."
-  length    = "1"
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "local_file" "name" {
-  filename = "demo.txt"
-  content = var.demo
+resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro" 
+  key_name               = "first"
+  vpc_security_group_ids = ["sg-0bb914c83ff842c27"]
+  tags = {
+    Name = "u21.local"
+  }
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo hostnamectl set-hostname U21.local
+  # netdata_conf="/etc/netdata/netdata.conf"
+  # Path to netdata.conf
+  # actual_ip=0.0.0.0
+  # Use sed to replace the IP address in netdata.conf
+  # sudo sed -i "s/bind socket to IP = .*$/bind socket to IP = $actual_ip/" "$netdata_conf"
+EOF
+
 }
-variable "demo" {
-    default = "name"
-    type = string
+
+resource "aws_instance" "frontend" { #amazon-playbook.yaml NGINX
+  ami                    = "ami-0230bd60aa48260c6"
+  instance_type          = "t2.micro"
+  key_name               = "first"
+  vpc_security_group_ids = ["sg-0bb914c83ff842c27"]
+  tags = {
+    Name = "c8.local"
+  }
+  user_data = <<-EOF
+  #!/bin/bash
+  # New hostname and IP address
+  sudo hostnamectl set-hostname u21.local
+  hostname=$(hostname)
+  public_ip="$(curl -s https://api64.ipify.org?format=json | jq -r .ip)"
+
+  # Path to /etc/hosts
+  echo "${aws_instance.backend.public_ip} $hostname" | sudo tee -a /etc/hosts
+
+EOF
+depends_on = [aws_instance.backend]
+}
+
+resource "local_file" "inventory" {
+  filename = "./inventory.yaml"
+  content  = <<EOF
+[frontend]
+${aws_instance.frontend.public_ip}
+[backend]
+${aws_instance.backend.public_ip}
+EOF
+}
+
+output "frontend_public_ip" {
+  value = aws_instance.frontend.public_ip
+}
+
+output "backend_public_ip" {
+  value = aws_instance.backend.public_ip
 }
